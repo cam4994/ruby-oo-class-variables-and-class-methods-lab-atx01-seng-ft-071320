dd08c89e8ffc29ded123d14435c825ea674626d9
Colin Mosley <colin.a.mosley@gmail.com>
2020-07-15 02:48:04 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song 
+  @@count = 0 
+  @@artists = [] 
+  @@genres = [] 
+  @@genre_count = {} 
+  @@artist_count = {} 
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+      @name = name 
+      @artist = artist 
+      @genre = genre 
+      @@count +=1 
+      @@artists << name 
+      if genre_count[genre]
+      @@genres << genre 
+      
+  end 
+  
+  
+  
+end 
